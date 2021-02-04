1e16c9a83e17786a13122886756b90d857a22ae9
Fuczek <kacper.dzwil@gmail.com>
2021-02-05 00:03:46 +0100

    adding spy stuff to check stuff

---
+++ levels.js
@@ -0,0 +1,76 @@
+const mongo = require('./mongo')
+const profileSchema = require('./schemas/profileSchema')
+let messagesCache = {}
+
+module.exports = (client) => {
+    setInterval(addToDatabase, 1000*3600)
+
+    client.on('message', message => {
+        const { guild, member } = message
+        countMessages(guild.id, member.id)
+/*         addXP(guild.id, member.id, 23) */
+    })
+}
+
+const addToDatabase = async () => {
+    console.log(messagesCache, 'interval')
+
+    await mongo().then(async mongoose => {
+        try {
+            for (const [userId, messagesAmount] of Object.entries(messagesCache)) {
+                console.log(`${userId}: ${messagesAmount}`);
+                const result = await profileSchema.findOneAndUpdate({
+                    userId
+                }, {
+                    userId,
+                    $inc: {
+                        messages: messagesAmount
+                    }
+                }, {
+                    upsert: true,
+                })
+            }
+            messagesCache = {}
+
+        }   finally {
+            mongoose.connection.close()
+        }
+    })
+}
+
+const countMessages = async (userId) => {
+    if (!messagesCache[userId]) {
+        messagesCache[userId] = 1
+    } else {
+        messagesCache[userId] += 1
+    }
+
+    console.log(messagesCache)
+}
+
+
+const addXP = async (guildId, userId, xpToAdd) => {
+    await mongo().then(async mongoose => {
+        try {
+            const result = await profileSchema.findOneAndUpdate({
+                guildId, 
+                userId
+            }, {
+                guildId,
+                userId,
+                $inc: {
+                    xp: xpToAdd
+                }
+            }, {
+                upsert: true,
+                new: true
+            })
+            console.log(result, 'result')
+
+        }   finally {
+            mongoose.connection.close()
+        }
+    })
+}
+
+module.exports.addXP = addXP
